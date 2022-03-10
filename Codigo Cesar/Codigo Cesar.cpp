cd6a25c4c3f59a1807dc5aec2172f654610896af
Fernando Garibay <garibaycejaf@gmail.com>
2022-03-10 11:08:38 -0600

    +Cifrado/Desifrado/Fuerza bruta

--- Codigo Cesar/Codigo Cesar.cpp
+++ Codigo Cesar/Codigo Cesar.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <ctype.h>  // isalpha, isupper
-#include <string.h> // strcspn
+#include <ctype.h>
+#include <string>
 
 using namespace std;
 
@@ -10,48 +10,114 @@ using namespace std;
 #define MAXIMA_LONGITUD_CADENA 5000
 #define MOD(i, n) (i % n + n) % n
 
-const char* alfabetoMinusculas = "abcdefghijklmnopqrstuvwxyz",
-          * alfabetoMayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+class CifradoCesar {
+private:
+    const char* alfabetoMinusculas = "abcdefghijklmnopqrstuvwxyz";
+    const char* alfabetoMayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+public:
+    CifradoCesar();
+    ~CifradoCesar();
 
-void cifrar(char* mensaje, char* destino, int rotaciones);
-int ord(char c);
+    char* cifrar(char*, int);
+    char* descifrar(char*, int);
+    char* fuerzaBruta(char*, int);
+    int enteroACaracter(char);
+};
 
-int main()
-{
-    char mensaje[] = "retirada";
-    char destino[200] = "";
-    cifrar(mensaje, destino, 3);
-    cout << destino << endl;
+CifradoCesar::CifradoCesar() {
 
-
-    return 0;
 }
 
-void cifrar(char* mensaje, char* destino, int rotaciones) {
+CifradoCesar::~CifradoCesar() {}
+
+char* CifradoCesar::cifrar(char* mensaje, int rotaciones) {
+    char* destino = new char[strlen(mensaje)];
+
     int i = 0;
     while (mensaje[i]) {
         char caracterActual = mensaje[i];
-        int posicionOriginal = ord(caracterActual);
+        int posicionOriginal = enteroACaracter(caracterActual);
+
+        if (!isalpha(caracterActual)) {
+            destino[i] = caracterActual;
+
+            i++;
+            continue;
+        }
+        if (isupper(caracterActual)) {
+            destino[i] = alfabetoMayusculas[(posicionOriginal - INICIO_ALFABETO_MAYUSCULAS + rotaciones) % LONGITUD_ALFABETO];
+        }
+        else {
+            destino[i] = alfabetoMinusculas[(posicionOriginal - INICIO_ALFABETO_MINUSCULAS + rotaciones) % LONGITUD_ALFABETO];
+        }
+        i++;
+    }
+    destino[strlen(mensaje)] = '\0';
+    return destino;
+}
+
+char* CifradoCesar::descifrar(char* mensaje, int rotaciones) {
+    char* destino = new char[strlen(mensaje)];
+
+    int i = 0;
+    while (mensaje[i]) {
+        char caracterActual = mensaje[i];
+        int posicionOriginal = enteroACaracter(caracterActual);
         if (!isalpha(caracterActual)) {
             destino[i] = caracterActual;
             i++;
             continue; // Ir a la siguiente iteración; por eso arriba aumentamos a i
         }
         if (isupper(caracterActual)) {
-            destino[i] =
-                alfabetoMayusculas[(posicionOriginal - INICIO_ALFABETO_MAYUSCULAS +
-                    rotaciones) %
-                LONGITUD_ALFABETO];
+            destino[i] = alfabetoMayusculas[MOD(
+                posicionOriginal - INICIO_ALFABETO_MAYUSCULAS - rotaciones,
+                LONGITUD_ALFABETO)];
         }
         else {
-
-            destino[i] =
-                alfabetoMinusculas[(posicionOriginal - INICIO_ALFABETO_MINUSCULAS +
-                    rotaciones) %
-                LONGITUD_ALFABETO];
+            destino[i] = alfabetoMinusculas[MOD(
+                posicionOriginal - INICIO_ALFABETO_MINUSCULAS - rotaciones,
+                LONGITUD_ALFABETO)];
         }
         i++;
     }
+    destino[strlen(mensaje)] = '\0';
+    return destino;
 }
 
-int ord(char c) { return (int)c; }
+char* CifradoCesar::fuerzaBruta(char* mensaje, int intervalo) {
+    return descifrar(mensaje, intervalo);
+}
+
+int CifradoCesar::enteroACaracter(char c) {
+    return (int)c;
+}
+
+
+int main()
+{
+    CifradoCesar objCifrado = CifradoCesar();
+
+    int rotaciones;
+    char mensaje[MAXIMA_LONGITUD_CADENA];
+    char* mensajeCifrado;
+    char* mensajeDesifrado;
+
+    cout << "Ingrese un mensaje a crifrar: ";
+    cin.getline(mensaje, MAXIMA_LONGITUD_CADENA);
+
+    cout << "Ingrese una cantidad de rotaciones: ";
+    cin >> rotaciones;
+
+    mensajeCifrado = objCifrado.cifrar(mensaje, rotaciones);
+    mensajeDesifrado = objCifrado.descifrar(mensajeCifrado, rotaciones);
+
+    cout << "\nMensaje cifrado: " << mensajeCifrado << endl;
+    cout << "Mensaje desifrado: " << mensajeDesifrado << endl;
+
+    cout << "\nMetodo de fuerza bruta -------" << endl;
+    for (int i = 1; i <= LONGITUD_ALFABETO; i++) {
+        cout << "Rotacion " << i << ": " << objCifrado.fuerzaBruta(mensajeCifrado, i) << endl;
+    }
+
+    return 0;
+}
